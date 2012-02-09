31bc13f8fb8052a06930fc4ca6f835077494d946
Kevin L. Mitchell <kevin.mitchell@rackspace.com>
2012-02-09 17:42:51 -0600

    Begin crafting turnstile distributed rate-limit middleware...

---
+++ turnstile/utils.py
@@ -0,0 +1,12 @@
+import sys
+
+
+def import_class(import_str):
+    """Returns a class from a string including module and class."""
+    mod_str, _sep, class_str = import_str.rpartition(':')
+    try:
+        __import__(mod_str)
+        return getattr(sys.modules[mod_str], class_str)
+    except (ImportError, ValueError, AttributeError) as exc:
+        # Convert it into an import error
+        raise ImportError("Failed to import %s: %s" % (import_str, exc))
