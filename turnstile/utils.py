fc9296957122fe7499711c0b0801c96e286b6725
Kevin L. Mitchell <kevin.mitchell@rackspace.com>
2012-02-10 17:42:27 -0600

    Add a context manager to ignore exceptions

--- turnstile/utils.py
+++ turnstile/utils.py
@@ -11,3 +11,17 @@ def import_class(import_str):
     except (ImportError, ValueError, AttributeError) as exc:
         # Convert it into an import error
         raise ImportError("Failed to import %s: %s" % (import_str, exc))
+
+
+class ignore_except(object):
+    """Context manager to ignore all exceptions."""
+
+    def __enter__(self):
+        """Entry does nothing."""
+
+        pass
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        """Return True to mark the exception as handled."""
+
+        return True
