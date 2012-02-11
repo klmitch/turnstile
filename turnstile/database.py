f3f4cfad01e473600b3bab030e2ee3b9c7842b11
Kevin L. Mitchell <kevin.mitchell@rackspace.com>
2012-02-10 19:32:02 -0600

    Change MapperDaemon to ControlDaemon.
    Adds documentation and generalizes the listener to allow other controls.
    Currently the only controls are reload and ping.

--- turnstile/database.py
+++ turnstile/database.py
@@ -1,4 +1,5 @@
 import logging
+import random
 import traceback
 
 import eventlet
@@ -95,61 +96,106 @@ def initialize():
     pass
 
 
-class MapperDaemon(object):
+class ControlDaemon(object):
     """
-    A daemon thread which listens for a reload event and reloads the
-    limit configuration from the database.
+    A daemon thread which listens for control messages and can reload
+    the limit configuration from the database.
     """
 
     def __init__(self, db, middleware, config):
+        """
+        Initialize the ControlDaemon.  Starts the listening thread and
+        triggers an immediate reload.
+        """
+
         # Save some relevant information
-        self.db = db
-        self.middleware = middleware
-        self.config = config
+        self._db = db
+        self._middleware = middleware
+        self._config = config
 
         # Need a semaphore to cover reloads in action
-        self.pending = eventlet.semaphore.Semaphore()
+        self._pending = eventlet.semaphore.Semaphore()
 
         # Spawn the listening thread
-        self.listen_thread = eventlet.spawn_n(self.listen)
+        self._listen_thread = eventlet.spawn_n(self._listen)
 
         # Now do the initial load
-        self.reload()
+        self._reload()
 
-    def listen(self):
+    def _listen(self):
+        """
+        Listen for incoming control messages.
+
+        If the 'redis.shard_hint' configuration is set, its value will
+        be passed to the pubsub() method when setting up the
+        subscription.  The control channel to subscribe to is
+        specified by the 'redis.control_channel' configuration
+        ('control' by default).
+        """
+
         # Need a pub-sub object
         kwargs = {}
-        if 'shard_hint' in self.config:
-            kwargs['shard_hint'] = self.config['shard_hint']
-        pubsub = self.db.pubsub(**kwargs)
+        if 'shard_hint' in self._config:
+            kwargs['shard_hint'] = self._config['shard_hint']
+        pubsub = self._db.pubsub(**kwargs)
 
-        # Subscribe to the right channel...
-        channel = self.config.get('reload_channel', 'reload')
+        # Subscribe to the right channel(s)...
+        channel = self._config.get('control_channel', 'control')
         pubsub.subscribe(channel)
 
         # Now we listen...
         for msg in pubsub.listen():
             # Only interested in messages to our reload channel
-            if (msg['type'] not in ('pmessage', 'message') or
-                msg['channel'] != channel):
-                continue
+            if (msg['type'] in ('pmessage', 'message') and
+                msg['channel'] == channel):
+                # Figure out what kind of message this is
+                command, _sep, args = msg['data'].partition(':')
 
-            # OK, trigger a reload
-            eventlet.spawn_n(self.reload)
+                # Don't do anything with internal commands
+                if command[0] == '_':
+                    continue
 
-    def reload(self):
+                # Don't do anything with missing commands
+                try:
+                    method = getattr(self, command)
+                except AttributeError:
+                    continue
+
+                # Don't do anything with non-callables
+                if not callable(method):
+                    continue
+
+                # Execute the desired command
+                try:
+                    method(*args.split(':'))
+                except TypeError:
+                    # Ignore if we don't have enough arguments
+                    pass
+
+    def _reload(self):
+        """
+        Reloads the limits configuration from the database.
+
+        If an error occurs loading the configuration, an error-level
+        log message will be emitted.  Additionally, the error message
+        will be added to the set specified by the 'redis.errors_key'
+        configuration ('errors' by default) and sent to the publishing
+        channel specified by the 'redis.errors_channel' configuration
+        ('errors' by default).
+        """
+
         # Acquire the pending semaphore.  If we fail, exit--someone
         # else is already doing the reload
-        if not self.pending.acquire(False):
+        if not self._pending.acquire(False):
             return
 
         # Do the remaining steps in a try/finally block so we make
         # sure to release the semaphore
         try:
             # Load all the limits
-            key = self.config.get('limits_key', 'limits')
+            key = self._config.get('limits_key', 'limits')
             limits = [limits.Limit.hydrate(self, msgpack.loads(lim))
-                      for lim in self.db.zrange(key, 0, -1)]
+                      for lim in self._db.zrange(key, 0, -1)]
 
             # Build the routes mapper
             mapper = routes.Mapper()
@@ -157,7 +203,7 @@ class MapperDaemon(object):
                 lim.route(mapper)
 
             # Install it
-            self.middleware.mapper = mapper
+            self._middleware.mapper = mapper
         except Exception:
             # Format an error message
             msg = traceback.format_exc()
@@ -166,18 +212,102 @@ class MapperDaemon(object):
             LOG.error("While reloading limits: %s" % msg)
 
             # Get our error set and publish channel
-            error_key = self.config.get('errors_key', 'errors')
-            error_channel = self.config.get('errors_channel', 'errors')
+            error_key = self._config.get('errors_key', 'errors')
+            error_channel = self._config.get('errors_channel', 'errors')
 
             # Store the message into the error set.  We use a set here
             # because it's likely that more than one node will
             # generate the same message if there is an error, and this
             # avoids an explosion in the size of the set.
             with utils.ignore_except():
-                self.db.sadd(error_key, msg)
+                self._db.sadd(error_key, msg)
 
             # Publish the message to a channel
             with utils.ignore_except():
-                self.db.publish(error_channel, msg)
+                self._db.publish(error_channel, msg)
         finally:
-            self.pending.release()
+            self._pending.release()
+
+    def ping(self, channel, data=None):
+        """
+        Process the 'ping' control message.
+
+        :param channel: The publish channel to which to send the
+                        response.
+        :param data: Optional extra data.  Will be returned as the
+                     second argument of the response.
+
+        Responds to the named channel with a command of 'pong' and
+        with the node_name (if configured) and provided data as
+        arguments.
+        """
+
+        if not channel:
+            # No place to reply to
+            return
+
+        # Get our configured node name
+        node_name = self._config.get('node_name')
+
+        # Format the response
+        reply = ['pong']
+        if node_name or data:
+            reply.append(node_name or '')
+        if data:
+            reply.append(data)
+
+        # And send it
+        with utils.ignore_except():
+            self._db.publish(channel, ':'.join(reply))
+
+    def reload(self, load_type=None, spread=None):
+        """
+        Process the 'reload' control message.
+
+        :param load_type: Optional type of reload.  If given as
+                          'immediate', reload is triggered
+                          immediately.  If given as 'spread', reload
+                          is triggered after a random period of time
+                          in the interval (0.0, spread).  Otherwise,
+                          reload will be as configured.
+        :param spread: Optional argument for 'spread' load_type.  Must
+                       be a float giving the maximum length of the
+                       interval, in seconds, over which the reload
+                       should be scheduled.  If not provided, falls
+                       back to configuration.
+
+        If a recognized load_type is not given, or is given as
+        'spread' but the spread parameter is not a valid float, the
+        configuration will be checked for the 'redis.reload_spread'
+        value.  If that is a valid value, the reload will be randomly
+        scheduled for some time within the interval (0.0,
+        redis.reload_spread).
+        """
+
+        # Figure out what type of reload this needs to be
+        if load_type == 'immediate':
+            spread = None
+        elif load_type == 'spread':
+            try:
+                spread = float(spread)
+            except (TypeError, ValueError):
+                # Not a valid float; use the configured spread value
+                load_type = None
+        else:
+            load_type = None
+
+        if load_type is None:
+            # Use configured set-up; see if we have a spread
+            # configured
+            try:
+                spread = float(self._config['reload_spread'])
+            except (TypeError, ValueError, KeyError):
+                # No valid configuration
+                spread = None
+
+        if spread:
+            # Apply a randomization to spread the load around
+            eventlet.spawn_after(random.random() * spread, self._reload)
+        else:
+            # Spawn in immediate mode
+            eventlet.spawn_n(self._reload)
