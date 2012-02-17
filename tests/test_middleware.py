756ea63c3da90e1c5d5cf774cc8a30e7c50573b6
Kevin L. Mitchell <kevin.mitchell@rackspace.com>
2012-02-16 18:25:22 -0600

    Start filling in tests on the middleware module

---
+++ tests/test_middleware.py
@@ -0,0 +1,29 @@
+from turnstile import database
+from turnstile import middleware
+
+import tests
+
+
+class TestHeadersDict(tests.TestCase):
+    def test_init_sequence(self):
+        hd = middleware.HeadersDict([('Foo', 'value'), ('bAR', 'VALUE')])
+
+        self.assertEqual(hd.headers, dict(foo='value', bar='VALUE'))
+
+    def test_init_dict(self):
+        hd = middleware.HeadersDict(dict(Foo='value', bAR='VALUE'))
+
+        self.assertEqual(hd.headers, dict(foo='value', bar='VALUE'))
+
+    def test_init_kwargs(self):
+        hd = middleware.HeadersDict(Foo='value', bAR='VALUE')
+
+        self.assertEqual(hd.headers, dict(foo='value', bar='VALUE'))
+
+    def test_get_item(self):
+        hd = middleware.HeadersDict(Foo='value')
+
+        self.assertEqual(hd['foo'], 'value')
+        self.assertEqual(hd['Foo'], 'value')
+        with self.assertRaises(KeyError):
+            foo = hd['bar']
