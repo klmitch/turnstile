dd936e5b2be32adeacbcfa5bc93a2e654b9460c7
Kevin L. Mitchell <kevin.mitchell@rackspace.com>
2012-03-02 15:14:14 -0600

    Version bump

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name='turnstile',
-    version='0.1',
+    version='0.2',
     author='Kevin L. Mitchell',
     author_email='kevin.mitchell@rackspace.com',
     description="Distributed rate-limiting middleware",
