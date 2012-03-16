690cd6351dd0fbf6af55f9c7cadce572b2b9f862
Kevin L. Mitchell <kevin.mitchell@rackspace.com>
2012-03-16 11:49:39 -0500

    Bump the version to alpha...

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name='turnstile',
-    version='0.5.1',
+    version='0.5.2a',
     author='Kevin L. Mitchell',
     author_email='kevin.mitchell@rackspace.com',
     description="Distributed rate-limiting middleware",
