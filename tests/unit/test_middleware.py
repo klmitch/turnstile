ea2890358e5fab17f5eb334ac44f9efdedca2fa8
Kevin L. Mitchell <kevin.mitchell@rackspace.com>
2013-03-01 17:52:53 -0600

    Move HeadersDict tests into unit subdirectory.

---
+++ tests/unit/test_middleware.py
@@ -0,0 +1,117 @@
+# Copyright 2013 Rackspace
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import unittest2
+
+from turnstile import middleware
+
+
+class TestHeadersDict(unittest2.TestCase):
+    def test_init_sequence(self):
+        hd = middleware.HeadersDict([('Foo', 'value'), ('bAR', 'VALUE')])
+
+        self.assertEqual(hd.headers, dict(foo='value', bar='VALUE'))
+
+    def test_init_dict(self):
+        hd = middleware.HeadersDict(dict(Foo='value', bAR='VALUE'))
+
+        self.assertEqual(hd.headers, dict(foo='value', bar='VALUE'))
+
+    def test_init_kwargs(self):
+        hd = middleware.HeadersDict(Foo='value', bAR='VALUE')
+
+        self.assertEqual(hd.headers, dict(foo='value', bar='VALUE'))
+
+    def test_get_item(self):
+        hd = middleware.HeadersDict(Foo='value')
+
+        self.assertEqual(hd['foo'], 'value')
+        self.assertEqual(hd['Foo'], 'value')
+        with self.assertRaises(KeyError):
+            foo = hd['bar']
+
+    def test_set_item(self):
+        hd = middleware.HeadersDict(Foo='value')
+
+        hd['fOO'] = 'bar'
+        self.assertEqual(hd.headers, dict(foo='bar'))
+        hd['bAr'] = 'blah'
+        self.assertEqual(hd.headers, dict(foo='bar', bar='blah'))
+
+    def test_del_item(self):
+        hd = middleware.HeadersDict(Foo='value', bAR='VALUE')
+
+        del hd['fOO']
+        self.assertEqual(hd.headers, dict(bar='VALUE'))
+        del hd['bar']
+        self.assertEqual(hd.headers, {})
+        with self.assertRaises(KeyError):
+            del hd['baz']
+
+    def test_contains(self):
+        hd = middleware.HeadersDict(Foo='value')
+
+        self.assertTrue('foo' in hd)
+        self.assertTrue('fOO' in hd)
+        self.assertFalse('bAR' in hd)
+
+    def test_iter(self):
+        hd = middleware.HeadersDict(Foo='value', bAR='VALUE')
+
+        result = sorted(list(iter(hd)))
+        self.assertEqual(result, ['bar', 'foo'])
+
+    def test_len(self):
+        hd = middleware.HeadersDict(Foo='value')
+
+        self.assertEqual(len(hd), 1)
+        hd['bAR'] = 'VALUE'
+        self.assertEqual(len(hd), 2)
+
+    def test_iterkeys(self):
+        hd = middleware.HeadersDict(Foo='value', bAR='VALUE')
+
+        result = sorted(list(hd.iterkeys()))
+        self.assertEqual(result, ['bar', 'foo'])
+
+    def test_iteritems(self):
+        hd = middleware.HeadersDict(Foo='value', bAR='VALUE')
+
+        result = sorted(list(hd.iteritems()))
+        self.assertEqual(result, [('bar', 'VALUE'), ('foo', 'value')])
+
+    def test_itervalues(self):
+        hd = middleware.HeadersDict(Foo='value', bAR='VALUE')
+
+        result = sorted(list(hd.itervalues()))
+        self.assertEqual(result, ['VALUE', 'value'])
+
+    def test_keys(self):
+        hd = middleware.HeadersDict(Foo='value', bAR='VALUE')
+
+        result = sorted(list(hd.keys()))
+        self.assertEqual(result, ['bar', 'foo'])
+
+    def test_items(self):
+        hd = middleware.HeadersDict(Foo='value', bAR='VALUE')
+
+        result = sorted(list(hd.items()))
+        self.assertEqual(result, [('bar', 'VALUE'), ('foo', 'value')])
+
+    def test_values(self):
+        hd = middleware.HeadersDict(Foo='value', bAR='VALUE')
+
+        result = sorted(list(hd.values()))
+        self.assertEqual(result, ['VALUE', 'value'])
